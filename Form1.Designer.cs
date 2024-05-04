3ae0f3a39c7ab2ba353b5c8c65c88c6f822975b8
anas12312 <anashesham000@gmail.com>
2024-05-04 16:18:36 +0300

    anas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,11 +29,11 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea3 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend3 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series3 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title3 = new System.Windows.Forms.DataVisualization.Charting.Title();
             System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea4 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
-            System.Windows.Forms.DataVisualization.Charting.Legend legend4 = new System.Windows.Forms.DataVisualization.Charting.Legend();
             System.Windows.Forms.DataVisualization.Charting.Series series4 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            System.Windows.Forms.DataVisualization.Charting.Title title4 = new System.Windows.Forms.DataVisualization.Charting.Title();
             this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.listBox1 = new System.Windows.Forms.ListBox();
             this.button1 = new System.Windows.Forms.Button();
@@ -41,40 +41,51 @@
             this.button3 = new System.Windows.Forms.Button();
             this.chart2 = new System.Windows.Forms.DataVisualization.Charting.Chart();
             this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.F_Score = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Eff = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.chart2)).BeginInit();
             this.SuspendLayout();
             // 
             // chart1
             // 
+            chartArea3.AxisX.Title = "Voltage (Volt)";
+            chartArea3.AxisY.Title = "Current (mA/cm^2)";
             chartArea3.Name = "ChartArea1";
             this.chart1.ChartAreas.Add(chartArea3);
-            legend3.Name = "Legend1";
-            this.chart1.Legends.Add(legend3);
             this.chart1.Location = new System.Drawing.Point(28, 98);
             this.chart1.Name = "chart1";
             series3.ChartArea = "ChartArea1";
             series3.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series3.Legend = "Legend1";
-            series3.Name = "Current(mA)";
+            series3.IsVisibleInLegend = false;
+            series3.Name = "c";
             this.chart1.Series.Add(series3);
             this.chart1.Size = new System.Drawing.Size(532, 387);
             this.chart1.TabIndex = 0;
             this.chart1.Text = "chart1";
+            title3.Name = "Title1";
+            title3.Text = "Voltage vs Current Density";
+            this.chart1.Titles.Add(title3);
             this.chart1.Click += new System.EventHandler(this.chart1_Click);
             // 
             // listBox1
             // 
             this.listBox1.FormattingEnabled = true;
             this.listBox1.ItemHeight = 16;
-            this.listBox1.Location = new System.Drawing.Point(68, 50);
+            this.listBox1.Location = new System.Drawing.Point(28, 24);
             this.listBox1.Name = "listBox1";
             this.listBox1.Size = new System.Drawing.Size(200, 20);
             this.listBox1.TabIndex = 1;
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(302, 47);
+            this.button1.Location = new System.Drawing.Point(302, 21);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -84,7 +95,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(393, 47);
+            this.button2.Location = new System.Drawing.Point(393, 21);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
@@ -94,43 +105,129 @@
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(485, 46);
+            this.button3.Location = new System.Drawing.Point(485, 20);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(75, 23);
             this.button3.TabIndex = 4;
-            this.button3.Text = "button3";
+            this.button3.Text = "Run";
             this.button3.UseVisualStyleBackColor = true;
             this.button3.Click += new System.EventHandler(this.button3_Click);
             // 
             // chart2
             // 
+            chartArea4.AxisX.Title = "Voltage (Volt)";
+            chartArea4.AxisY.Title = "Power (Watt)";
             chartArea4.Name = "ChartArea1";
             this.chart2.ChartAreas.Add(chartArea4);
-            legend4.Name = "Legend1";
-            this.chart2.Legends.Add(legend4);
             this.chart2.Location = new System.Drawing.Point(586, 98);
             this.chart2.Name = "chart2";
             series4.ChartArea = "ChartArea1";
             series4.ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Spline;
-            series4.Legend = "Legend1";
-            series4.Name = "Power(W)";
+            series4.Name = "p";
             this.chart2.Series.Add(series4);
             this.chart2.Size = new System.Drawing.Size(572, 387);
             this.chart2.TabIndex = 5;
             this.chart2.Text = "chart2";
+            title4.Name = "Title1";
+            title4.Text = "Voltage vs Power";
+            this.chart2.Titles.Add(title4);
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(1058, 50);
+            this.textBox1.Location = new System.Drawing.Point(1058, 24);
             this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
             this.textBox1.Size = new System.Drawing.Size(100, 22);
             this.textBox1.TabIndex = 6;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(977, 23);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "On";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(896, 24);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "Off";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(28, 69);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 9;
+            this.button6.Text = "Export";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(586, 25);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(332, 540);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "F_Score";
+            // 
+            // F_Score
+            // 
+            this.F_Score.AutoSize = true;
+            this.F_Score.Location = new System.Drawing.Point(410, 540);
+            this.F_Score.Name = "F_Score";
+            this.F_Score.Size = new System.Drawing.Size(44, 16);
+            this.F_Score.TabIndex = 13;
+            this.F_Score.Text = "label2";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(332, 589);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "Efficiency";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Eff
+            // 
+            this.Eff.AutoSize = true;
+            this.Eff.Location = new System.Drawing.Point(410, 589);
+            this.Eff.Name = "Eff";
+            this.Eff.Size = new System.Drawing.Size(44, 16);
+            this.Eff.TabIndex = 15;
+            this.Eff.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1245, 516);
+            this.ClientSize = new System.Drawing.Size(1189, 654);
+            this.Controls.Add(this.Eff);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.F_Score);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.chart2);
             this.Controls.Add(this.button3);
@@ -156,6 +253,14 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.DataVisualization.Charting.Chart chart2;
         private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label F_Score;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label Eff;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
 using System.Text;
@@ -15,11 +16,19 @@ namespace GUI_Serial_Interface
     public partial class Form1 : Form
     {
         SerialPort port;
+        private float area = 10;
+        private float maxI = 0;
+        private float maxV = 0;
+        private double maxP = 0;
+        private List<float> iBuffer = new List<float>();
+        private List<float> vBuffer = new List<float>();
+        private List<double> pBuffer = new List<double>();
         public Form1()
         {
             InitializeComponent();
             this.FormClosed += new FormClosedEventHandler(Form1_FormClosed);
             PopulateSerialPortList();
+            NotConnected();
         }
         private void PopulateSerialPortList()
         {
@@ -56,13 +65,82 @@ namespace GUI_Serial_Interface
                 textBox1.Text = text;
             }
         }
+        private void Finished()
+        {
+            double v = maxP / (maxI * maxV * 0.001);
+            double e = (maxP * 10) / area;
+            F_Score.Text = v.ToString();
+            Eff.Text = e.ToString();
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = true;
+            textBox1.Enabled = true;
+            listBox1.Enabled = true;
+        }
+        private void Running()
+        {
+            F_Score.Text = String.Empty;
+            Eff.Text = String.Empty;
+            button2.Enabled = false;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            textBox1.Enabled = false;
+            listBox1.Enabled = false;
+        }
+        private void Connected()
+        {
+            button2.Enabled = true;
+            button3.Enabled = true;
+            button4.Enabled = true;
+            button5.Enabled = true;
+            button6.Enabled = false;
+            textBox1.Enabled = true;
+            listBox1.Enabled = true;
+        }
+        private void NotConnected()
+        {
+            button2.Enabled = true;
+            listBox1.Enabled = true;
+            button3.Enabled = false;
+            button4.Enabled = false;
+            button5.Enabled = false;
+            button6.Enabled = false;
+            textBox1.Enabled = false;
+        }
         private void port_DataRecieved(object sender, SerialDataReceivedEventArgs e)
         {
             String receivedData = port.ReadLine();
             String[] data = receivedData.Split(';');
             Console.WriteLine(receivedData);
+            if (data[0] == "x")
+            {
+                Console.WriteLine("Done");
+                this.Invoke(new MethodInvoker(delegate () {
+                    Finished();
+                }));
+                return;
+            }
             float x = float.Parse(data[0]);
             float y = float.Parse(data[1]);
+            if(x > maxV)
+            {
+                maxV = x;
+            }
+            if(y > maxI)
+            {
+                maxI = y;
+            }
+            if(x * y * 0.001 > maxP)
+            {
+                maxP = x * y * 0.001;
+            }
+            iBuffer.Add(x);
+            vBuffer.Add(y);
+            pBuffer.Add(x * y * 0.001);
             UpdateTextField(data[2]);
             AddPointToChart(TrucatedValue(x), TrucatedValue(y));
         }
@@ -78,8 +156,8 @@ namespace GUI_Serial_Interface
                 });
             }else
             {
-                chart1.Series["Current(mA)"].Points.AddXY(x, y);
-                chart2.Series["Power(W)"].Points.AddXY(x, x * y * 0.001);
+                chart1.Series["c"].Points.AddXY(x, y/area);
+                chart2.Series["p"].Points.AddXY(x, x * y * 0.001);
             }
         }
         void Form1_FormClosed(object sender, FormClosedEventArgs e)
@@ -102,15 +180,93 @@ namespace GUI_Serial_Interface
                 port = new SerialPort("COM3", 9600);
                 port.DataReceived += port_DataRecieved;
                 port.Open();
+                Connected();
             }
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            port.WriteLine("a");
+            if(port != null)
+            {
+                try
+                {
+                    area = float.Parse(textBox2.Text);
+                } catch(Exception)
+                {
+
+                }
+                if(area > 0)
+                {
+                    Running();
+                    port.WriteLine("a");
+                }
+            }
         }
 
         private void chart1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(port != null)
+            {
+                port.WriteLine("o");
+            }
+        }
+        private void ExportToCSV(string csvFilePath)
+        {
+
+            if (String.IsNullOrWhiteSpace(csvFilePath))
+            {
+                return;
+            }
+
+            var csv = new StringBuilder();
+
+            csv.AppendLine("V,I,P");
+
+
+            var dataCount = iBuffer.Count;
+
+            for (int i = 0; i < dataCount; i++)
+            {
+                csv.AppendLine($"{vBuffer[i]}, {iBuffer[i]}, {pBuffer[i]}");
+            }
+
+            File.WriteAllText(csvFilePath + "/Export.csv", csv.ToString());
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if(port != null)
+            {
+                port.WriteLine("f");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    string[] files = Directory.GetFiles(fbd.SelectedPath);
+                    ExportToCSV(fbd.SelectedPath);
+                }
+            }
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
         {
 
         }
